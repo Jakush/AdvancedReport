8a3a814bc0722476563e201c868cb08ce82362b7
RETAMROVEC <James_Sulc@outlook.cz>
2023-03-18 16:01:31 +0100

    Commit

---
+++ src/main/java/me/retamrovec/advancedreport/AdvancedReport.java
@@ -0,0 +1,17 @@
+package me.retamrovec.advancedreport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedReport extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
